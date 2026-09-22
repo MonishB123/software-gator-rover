e88eeca0222c28069345984211b02fe29d0c1937
Alex Rivera <danyalex0607@gmail.com>
2026-09-22 15:13:06 -0400

    updated the linux docker config to use devcontainer

--- .devcontainer/linux/run.sh
+++ .devcontainer/linux/run.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-
-xhost +local:root
-
-docker run -it \
-    --net=host \
-    --ipc=host \
-    -e DISPLAY=$DISPLAY \
-    gatorrover:latest
     
+devcontainer exec --workspace-folder . --config .devcontainer/linux/devcontainer.json bash
+
+
