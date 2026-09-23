a930b91420dc3c33171c465e70235cbca0b40c3a
Alex Rivera <danyalex0607@gmail.com>
2026-09-22 20:42:55 -0400

    devcontainer and dockerfile changes and linux/windows split

--- .devcontainer/linux/run.sh
+++ .devcontainer/linux/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
     
-devcontainer exec --workspace-folder . --config .devcontainer/linux/devcontainer.json bash
+devcontainer up --workspace-folder . \
+    --config .devcontainer/linux/devcontainer.json \
+    --mount 'type=bind,source=/home/alexrivera/.config/nvim,target=/root/.config/nvim' \
+    --additional-features '{"ghcr.io/duduribeiro/devcontainer-features/neovim:1": { "version": "stable" }, "ghcr.io/devcontainers/features/sshd:1": {} }'
 
 
